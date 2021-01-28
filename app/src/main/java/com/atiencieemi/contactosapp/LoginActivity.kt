bb371b461c1a52f1ca64bef7c0e8f108e941fa3c
Emi Atiencie <58490441+Remilio11@users.noreply.github.com>
2021-01-27 19:23:32 -0500

    Guardar datos de Usuario

--- app/src/main/java/com/atiencieemi/contactosapp/LoginActivity.kt
+++ app/src/main/java/com/atiencieemi/contactosapp/LoginActivity.kt
@@ -1,18 +1,30 @@
 package com.atiencieemi.contactosapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     lateinit var buttonLogin : Button
     lateinit var editTextTextEmailAddress : EditText
     lateinit var editTextTextPassword : EditText
+    lateinit var checkBoxRecordarme : CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+
+        //Lectura de valores de archivos de preferencia si existen
+        editTextTextEmailAddress.setText ( sharedPref.getString(LOGIN_KEY,"") )
+        editTextTextPassword.setText ( sharedPref.getString(PASSWORD_KEY,"") )
+
+        //Inicizliacion de variables
         buttonLogin = findViewById<Button>(R.id.buttonLogin)
         editTextTextEmailAddress = findViewById<EditText>(R.id.editTextTextEmailAddress)
         editTextTextPassword = findViewById<EditText>(R.id.editTextTextPassword)
@@ -20,6 +32,18 @@ class LoginActivity : AppCompatActivity() {
         buttonLogin.setOnClickListener {
             if (!ValidarDatos())
                 return@setOnClickListener
+            if(checkBoxRecordarme.isChecked){
+                val editor = sharedPref.edit()
+                editor.putString(LOGIN_KEY,editTextTextEmailAddress.text.toString())
+                editor.putString(PASSWORD_KEY,editTextTextPassword.text.toString())
+                editor.commit()
+            }
+            else{
+                val editor = sharedPref.edit()
+                editor.putString(LOGIN_KEY,"")
+                editor.putString(PASSWORD_KEY,"")
+                editor.commit()
+            }
         }
     }
     fun ValidarDatos(): Boolean {
